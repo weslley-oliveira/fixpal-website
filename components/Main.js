6ffbbdeeb757c740b91ee1d05833112ac7f1ebe9
Weslley Oliveira <weslleyzera2020@gmail.com>
2023-04-23 13:50:34 +0100

    nav color

--- components/Main.js
+++ components/Main.js
@@ -11,10 +11,20 @@ export default function Main() {
           </p>
           <div className="flex justify-center">
             <a
-              className="inline-flex items-center px-5 py-3 mt-2 font-medium text-white transition duration-500 ease-in-out transform bg-transparent border rounded-lg bg-gray-900"
+              className="inline-flex items-center px-5 py-3 mt-2 bgLaranja text-black font-bold  transition duration-500 ease-in-out transform bg-transparent border rounded-lg bg-gray-900"
               href="https://fixpal.co.uk"
             >
               <span className="justify-center">Fix my home</span>
+              <svg
+                    className="w-3 h-3 fill-current flex ml-2 -mr-1"
+                    viewBox="0 0 12 12"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                <path
+                  d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z"
+                  fillRule="nonzero"
+                />
+              </svg>
             </a>
           </div>
         </div>
